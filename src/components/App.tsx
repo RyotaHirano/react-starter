377a67125fe9d23667b6b9b2801bb66edf8b9658
Ryota Hirano <hirano@tsumikiinc.com>
2019-01-10 22:51:36 +0900

    add styled-components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 // normalize
 import "normalize.css/normalize.css"
+import styled from 'styled-components'
 
 export type AppProps = {
   name: string
 }
 
 export const App = (props: AppProps) => (
-  <p>
+  <P>
     This is { props.name } Component.
-  </p>
+  </P>
 )
+
+const P = styled.p`
+  font-size: 1.0rem;
+  padding: 20px;
+`
