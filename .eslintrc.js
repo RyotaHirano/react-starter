38a4872ef0ea7e9bc749a05d570e91570a0a7c9e
RyotaHirano <hirano@tsumikiinc.com>
2019-03-03 00:19:19 +0900

    replace .eslintrc with .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
-{
-  "extends": [
+module.exports = {
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:prettier/recommended"
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json"
   },
-  "plugins": [
+  plugins: [
     "@typescript-eslint"
   ],
-  "env": {
-    "es6": true,
-    "browser": true,
-    "node": true
+  env: {
+    es6: true,
+    browser: true,
+    node: true
   },
-  "rules": {
+  rules: {
     "node/no-unsupported-features/es-syntax": "off",
     "no-dupe-class-members": "off",
     "no-unused-vars": "off",
@@ -29,12 +29,14 @@
     "@typescript-eslint/prefer-namespace-keyword": "error",
     "@typescript-eslint/no-var-requires": "error",
     "@typescript-eslint/no-object-literal-type-assertion": "error",
+    // "@typescript-eslint/no-unnecessary-type-assertion": "error", // prettier error
+    // "@typescript-eslint/restrict-plus-operands": "error", // prettier error
     "react/display-name": "off",
     "react/prop-types": "off"
   },
-  "settings": {
-    "node": {
-      "tryExtensions": [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
+  settings: {
+    node: {
+      tryExtensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
     }
   }
 }
